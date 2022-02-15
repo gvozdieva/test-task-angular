69ab9c1a117e98bec50f56b37ea741de09149c0d
Anastasiia Hvozdieva <Anastasiia.Hvozdieva@nice.com>
2022-02-15 14:57:17 +0200

    added some service tests

--- src/app/cards.service.spec.ts
+++ src/app/cards.service.spec.ts
@@ -1,1 +1,21 @@
-// describe('')
+import { CardsService } from './cards.service'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {TestBed} from "@angular/core/testing";
+
+describe('CardsService', () => {
+let service: CardsService;
+
+  beforeEach(() => {
+    service = new CardsService();
+  });
+
+  it('should be created', () => {
+    const service = new CardsService;
+    expect(service).toBeTruthy();
+  });
+
+  it('should have getData function', () => {
+
+    expect(service.getById).toBeTruthy();
+  });
+})
